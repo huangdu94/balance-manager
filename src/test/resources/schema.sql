c3054d8da22d3c4f8169cad386d9ab02b04eb956
依韻 <huangdu.hd@alibaba-inc.com>
2024-02-23 22:37:21 +0800

    test 配置

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,32 @@
+-- 项目启动时该脚本自动执行，无需手动执行
+-- 用户表
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  name TEXT NOT NULL
+);
+INSERT INTO users (name) VALUES ('alice'), ('bob'), ('charlie');
+
+-- 额度类型表
+DROP TABLE IF EXISTS limit_types;
+CREATE TABLE limit_types (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  name TEXT NOT NULL
+);
+-- 信用卡/贷款/透支/投资/购物
+INSERT INTO limit_types (name) VALUES ('credit'), ('loan'), ('overdraft'), ('investment'), ('spending');
+
+-- 额度表
+DROP TABLE IF EXISTS limits;
+CREATE TABLE limits (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  user_id INTEGER,
+  limit_type_id INTEGER,
+  amount REAL
+);
