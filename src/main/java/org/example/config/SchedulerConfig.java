69c609686376244d0213aa398d833c4e1b7d349b
依韻 <huangdu.hd@alibaba-inc.com>
2024-02-24 21:37:26 +0800

    启用定时任务支持

---
+++ src/main/java/org/example/config/SchedulerConfig.java
@@ -0,0 +1,14 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 启用定时任务支持
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
